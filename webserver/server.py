d5e6929487b0263a5aa635aa57ca7e77a5c5c4e9
KunyiLiu <kl2912@columbia.edu>
2017-06-24 16:35:19 +0800

    Update server.py

--- webserver/server.py
+++ webserver/server.py
@@ -35,7 +35,6 @@ def teardown_request(exception):
     g.conn.close()
   except Exception as e:
     pass
-
  
 # @app.route is a decorator around index() that means:
 #   run index() whenever the user tries to access the "/" path using a GET request
@@ -45,25 +44,15 @@ def index():
   
   
 if __name__ == "__main__":
-
   import click
-
-
-
   @click.command()
-
   @click.option('--debug', is_flag=True)
-
   @click.option('--threaded', is_flag=True)
-
   @click.argument('HOST', default='0.0.0.0')
-
   @click.argument('PORT', default=8111, type=int)
 
   def run(debug, threaded, host, port):
-
     """
-
     This function handles command line parameters.
 
     Run the server using
@@ -76,12 +65,8 @@ if __name__ == "__main__":
 
     """
 
-
-
     HOST, PORT = host, port
-
     print "running on %s:%d" % (HOST, PORT)
-
     app.run(host=HOST, port=PORT, debug=debug, threaded=threaded)
 
 
